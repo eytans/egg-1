ce48c5a3d4a0b4c97526c4e78a64d75cc32cdf8e
Eytan Singher <eytan.singher@gmail.com>
2024-10-06 13:19:14 +0300

    Also into usize

--- src/lib.rs
+++ src/lib.rs
@@ -79,9 +79,21 @@ impl Into<u32> for Id {
     }
 }
 
+impl Into<usize> for Id {
+    fn into(self) -> usize {
+        self.0 as usize
+    }
+}
+
 impl Into<u32> for &Id {
     fn into(self) -> u32 {
         self.0
+    }
+}
+
+impl Into<usize> for &Id {
+    fn into(self) -> usize {
+        self.0 as usize
     }
 }
 
